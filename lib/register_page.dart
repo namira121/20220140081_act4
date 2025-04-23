e9210268968da90fc8ae1e452d2a0756f0b9c03f
Namira Pramadhani <namnam6617@gmail.com>
2025-04-23 21:50:13 +0700

    menambahkan textfield untuk input email dan password. serta menambahkan textbutton untuk kembali ke loginpage

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -25,7 +25,34 @@ class _RegisterPageState extends State<RegisterPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            
+            Text('Register'),
+            TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value){
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value){
+                  if(value == null || value.isEmpty){
+                    return 'Please enter your password';
+                  }
+                  return null;
+                },
+              ),
+              ElevatedButton(onPressed: (){}, child: Text('Register')
+              ),
+              TextButton(onPressed: (){
+                Navigator.pushNamed(
+                  context, '/login');
+              }, child: Text('Sudah punya akun? Login Sekarang!'),)
           ],
         ),)),
     );
