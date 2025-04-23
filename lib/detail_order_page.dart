ac9d3f8a23d8bf74cf80f7efe00478aee8608a15
Namira Pramadhani <namnam6617@gmail.com>
2025-04-23 22:50:51 +0700

    membuat variabel untuk menyimpan nilai makanan, minuman, jumlah dan total harga

--- lib/detail_order_page.dart
+++ lib/detail_order_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
 class DetailOrderPage extends StatelessWidget {
-  const DetailOrderPage({super.key});
+  final String makanan;
+  final String minuman;
+  final String jumlahMakanan;
+  final String jumlahMinuman;
+  final int totalHarga;
+
+  const DetailOrderPage({
+    super.key,
+    required this.makanan,
+    required this.minuman,
+    required this.jumlahMakanan,
+    required this.jumlahMinuman,
+    required this.totalHarga,
+  });
 
   @override
   Widget build(BuildContext context) {
