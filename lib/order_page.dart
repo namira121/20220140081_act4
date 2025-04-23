d155a0fe911296ddc79c4fa726fa3e57f3779bc0
Namira Pramadhani <namnam6617@gmail.com>
2025-04-23 22:15:48 +0700

    menambahkan appbar untuk form orderpage

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -25,6 +25,18 @@ class _OrderPageState extends State<OrderPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final _formKey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(title: Text('Order Page')),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          spacing: 16,
+          children: [
+            
+          ],
+        ),
+      ),
+    );
   }
 }
