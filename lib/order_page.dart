dcc520e1108dac936299a9530ebeba350ec097e2
Namira Pramadhani <namnam6617@gmail.com>
2025-04-23 22:12:51 +0700

    membuat fungsi untuk menghitung harga makanan dan minuman

--- lib/order_page.dart
+++ lib/order_page.dart
@@ -13,7 +13,16 @@ class _OrderPageState extends State<OrderPage> {
   final TextEditingController jumlahMakananController = TextEditingController();
   final TextEditingController jumlahMinumanController = TextEditingController();
   int totalHarga = 0;
-  
+
+  void calculateTotalPrice(){
+    int jumlahMakanan = int.tryParse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.tryParse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman *5000);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
