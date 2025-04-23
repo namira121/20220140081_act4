b0dcd9cd97afa486ff7667051d58e3b53b49524e
Namira Pramadhani <namnam6617@gmail.com>
2025-04-23 20:52:22 +0700

    membuat Column untuk wadah textformfield

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -19,7 +19,10 @@ class _LoginPageState extends State<LoginPage> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
-            
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              
+            ],
           ),))
     );
     
